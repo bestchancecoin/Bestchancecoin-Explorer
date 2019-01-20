bcac71fe297d084a2d0023e88fbbacf59d81c94e
bestchancecoin <45109769+bestchancecoin@users.noreply.github.com>
2019-01-20 13:19:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-var api = 'http://seed79.cryptoluka.cl:52421';
+var api = 'http://85.255.12.170:18236';
 // var api = 'http://35.231.66.169:8888';
 
 var blockTargetInterval = 90;
 var coinUnits = 100000000;
-var symbol = 'LUK';
+var symbol = 'BCC';
 var refreshDelay = 30000;
 
 // Pools Stats by MainCoins
 var networkStat = {
     "luk": [
-		["Pool Official", "http://pool.cryptoluka.cl:9995"]
+		["Pool Official", "http://pool.bestchancecoin.com:8117"]
 		
     ]
 };
